b63476991088afbeb8209bec97b8438773aa7834
Meachelle <abellameachelleabella2988@gmail.com>
2024-07-21 16:21:06 +0800

    Creation of Repo
    This is where we started and created the application

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import "./Navbar.css"
+
+const Navbar = () => {
+    return ( 
+        <nav className="navbar">
+            <div className="container">
+                <h1>Hello this is Navbar</h1>
+            </div>
+        </nav>
+     );
+}
+ 
+export default Navbar;
